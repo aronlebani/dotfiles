b1af336627eef3c9b40b8e1db19613fb42256415
Aron <aron.lebani@gmail.com>
2019-02-06 15:26:12 +1030

    Add install script

--- install.sh
+++ install.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-# Install atom packages
-apm install --packages ./.atom/packages.txt
+function install() {
+  rsync --exclude ".git/" \
+        --exclude "install.sh" \
+        --exclude "README.md" \
+        -av . ~
+
+  # Atom
+  if command -v apm > /dev/null 2>&1; then
+    echo "Installing Atom plugins..."
+    apm install --packages ./.atom/packages.txt
+  fi
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  install
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    install
+  fi
+fi
+
+unset install
+source ~/.bash_profile
