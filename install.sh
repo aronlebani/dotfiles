f6e99bfb810936f5b45e4d57de34afd021e279fb
aron <aron.lebani@gmail.com>
2019-02-21 10:56:16 +1030

    Add outline for a script to install software packages using snap. Rename installation of dotfiles from install to sync.

--- install.sh
+++ install.sh
@@ -1,32 +1,20 @@
-#!/bin/bash
+# Snap
+apt install snapd
 
-function install() {
-  if [ ! -d ~/.logs ]; then
-    mkdir ~/.logs
-    chmod 700 ~/.logs
-  fi
-  
-  rsync --exclude ".git/" \
-        --exclude "install.sh" \
-        --exclude "README.md" \
-        -av . ~
+# Atom
+snap install atom
 
-  # Atom
-  if command -v apm > /dev/null 2>&1; then
-    echo "Installing Atom plugins..."
-    apm install --packages ./.atom/packages.txt
-  fi
-}
+# Chrome
+snap install chromium
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  install
-else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    install
-  fi
-fi
+# Datagrip
+snap install datagrip
 
-unset install
-source ~/.bash_profile
+# Postman
+snap install postman
+
+# Gitkraken
+snap install gitkraken
+
+# Spotify
+snap install spotify
---
+++ sync.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function sync() {
+  if [ ! -d ~/.logs ]; then
+    mkdir ~/.logs
+    chmod 700 ~/.logs
+  fi
+
+  rsync --exclude ".git/" \
+        --exclude "install.sh" \
+        --exclude "README.md" \
+        -av . ~
+
+  # Atom
+  if command -v apm > /dev/null 2>&1; then
+    echo "Installing Atom plugins..."
+    apm install --packages ./.atom/packages.txt
+  fi
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  sync
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sync
+  fi
+fi
+
+unset install
+source ~/.bash_profile
