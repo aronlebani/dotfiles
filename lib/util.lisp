69dae6252da66307792306265a83d797e20dde6f
Aron Lebani <aron@lebani.dev>
2024-02-28 22:58:03 +1100

    Add gitinstall script, refactor some lib code

--- lib/util.lisp
+++ lib/util.lisp
@@ -6,6 +6,13 @@
 ;;;; These utilities are intended to be small and require little to no maintenance, so that they
 ;;;; can simply be copy-pasted into the source files that use them.
 
+;;; Common macros
+
+(defmacro with-gensyms ((&rest names) &body body)
+  "Binds each named variable to a gensymed symbol."
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
 ;;; Static type-checking
 
 (deftype -> (args &optional values)
@@ -16,6 +23,8 @@
 (defmacro -> (function (&rest args) &optional values)
   "Declaim the ftype of FUNCTION from ARGS to VALUES.
 
+   Examples:
+   
    (-> add (integer integer) integer)
    (defun add (x y) (+ x y))"
   `(declaim (ftype (-> (,@args) ,@(when values
@@ -25,68 +34,16 @@
 ;;; Calculations
 
 (defun rgb->hex (r g b &key pretty?)
+  "Convert an RGB value to hex. If 'pretty?' is 't', the result is pretty-printed."
   (let ((out `(,(format nil "~X" r) ,(format nil "~X" r) ,(format nil "~X" r))))
     (if pretty?
         (format nil "#~X~X~X" (first out) (second out) (third out))
         out)))
 
 (defun px->rem (px &key pretty?)
+  "Convert a value in units of pixels to units of rem. If 'pretty?' is 't', the
+   result is pretty-printed."
   (let ((out (float (/ px 16))))
     (if pretty?
         (format nil "~arem" out) 
         out)))
-
-;;; Pathname
-
-(defun exists? (value)
-  "Checks whether the given value exists."
-  (and value (not (eql value :unspecific))))
-
-(defun path-is-directory? (path)
-  "Checks if the given pathname is a directory."
-  (and
-    (not (exists? (pathname-name path)))
-    (not (exists? (pathname-type path)))
-    path))
-
-(defun path-to-directory (path)
-  "Converts a pathname to directory form."
-  (let ((pathname (pathname path)))
-    (when (wild-pathname-p pathname)
-      (error "Can't reliably convert wild pathnames."))
-    (if (not (path-is-directory? path))
-        (make-pathname
-          :directory (append (or (pathname-directory pathname) (list :relative))
-                             (list (file-namestring pathname)))
-          :name nil
-          :type nil
-          :defaults pathname)
-        pathname)))
-
-(defun path-to-wildcard (path)
-  "Returns a wildcard pathname for the given pathname."
-  (make-pathname
-    :name :wild
-    :type :wild
-    :defaults (path-to-directory path)))
-
-(defun path-list (path)
-  "Lists the contents of the given pathname."
-  (when (wild-pathname-p path)
-    (error "Can only list concrete directory names."))
-  (directory (path-to-wildcard path)))
-
-(defun walk-directory (path &key file-fn directory-fn (test (constantly t)))
-  "Walk a directory tree. Applies the provided function on all pathnames under the directory. If
-   :test is provided, only calls the function on the pathnames where :test returns true."
-  (labels
-    ((walk (name)
-       (cond
-         ((path-is-directory? name)
-          (if (and directory-fn (funcall test name))
-              (funcall directory-fn name))
-          (dolist (x (path-list name)) (walk x)))
-         (t
-          (if (and file-fn (funcall test name))
-              (funcall file-fn name))))))
-    (walk (path-to-directory path))))
